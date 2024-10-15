5f14e039253f5abc4c1371b5ad569ee6df9dedaa
HARRIS GOMEZ <harrisgomez@macbookpro.mynetworksettings.com>
2024-10-15 16:02:11 -0400

    Remove all instances of .DS_store and update .gitignore

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,38 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// configures HTTP methods to be READ-ONLY to limit API exposure
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        // disables HTTP methods for Products: PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // now, only the GET method will work for the Products repository
+
+        // do the same thing for the ProductCategory repository
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}
